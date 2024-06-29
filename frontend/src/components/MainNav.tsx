6accf71233650511aff06f9e64c7668bbe743dfb
Vaibtan <vaibhav21296@iiitd.ac.in>
2024-06-30 01:03:11 +0530

    one_way

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,12 +1,23 @@
+import UsernameMenu from "./UsernameMenu";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const MainNav = () => {
+    const {loginWithRedirect, isAuthenticated} = useAuth0();
     return (
-        <Button 
-            variant = "ghost" 
-            className = "font-bold text-2xl hover:text-orange-500 hover:bg-white">
-                Login 
-        </Button>
+        <span className = "flex space-x-2 items-center">
+            {isAuthenticated ? (
+                <UsernameMenu />
+             ) : (
+                <Button 
+                    variant = "ghost" 
+                    className = "font-bold text-2xl hover:text-orange-500 hover:bg-white"
+                    onClick = {async () => await loginWithRedirect()}
+                >
+                    Login 
+                </Button>
+            )}
+        </span>
     );
 };
 
